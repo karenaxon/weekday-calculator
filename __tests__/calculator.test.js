ecfff3151aa58710e5c0f61130bc92c268631aa4
Kaxon <karenaxon@users.noreply.github.com>
2021-11-11 14:50:01 -0800

    Add second Calculator test.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,9 +1,13 @@
 import Calculator from './../src/js/Calculator.js'
 
 describe('Calculator', () => {
+  let date1;
+  
+  beforeEach(() => {
+    date1 = new Calculator("3");
+  });
 
-  test('It should return 3 as the date', () => {
-    const date1 = new Calculator(3);
-    expect(date1.date).toEqual(3);
+  test('It should return true after invoking the getDay function', () => {
+    expect(date1.day).toBeTruthy();
   });
 });
--- src/js/Calculator.js
+++ src/js/Calculator.js
@@ -1,6 +1,11 @@
 export default class Calculator {
-  
+
   constructor(date) {
     this.date = date;
+    this.day = this.getDay(date);
+  }
+
+  getDay(string) {
+    return true;
   }
 }
